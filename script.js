d00abb51b179f8b6d6c1025f0f7f4798b68bf325
Zan0syy <murradov.denis@yandex.ru>
2023-06-11 22:17:02 +0500

    fishGame

---
+++ script.js
@@ -0,0 +1,15 @@
+function change(change){
+    
+}
+function loca(el){
+    el.innerHTML="Локация 1";
+    document.getElementById('loc_img').src="http://gonefishing.narod.ru/ezhov1024.jpg";
+    console.log('lokacia 1')
+}
+     
+function loc(ele){
+    ele.innerHTML='Локация 2';
+    document.getElementById('loc_img').src='http://gonefishing.narod.ru/yalozero1024.jpg';
+    console.log('Локация 2')
+}
+console.log('kuk')
